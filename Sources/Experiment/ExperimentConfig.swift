6a0667c8e19b13dffbea89645348dd70fc2311ae
Brian Giori <brian.giori@amplitude.com>
2021-06-02 01:03:44 -0700

    update version to 0.0.1

--- Sources/Experiment/ExperimentConfig.swift
+++ Sources/Experiment/ExperimentConfig.swift
@@ -100,7 +100,7 @@ public struct ExperimentConfig {
     internal struct Constants {
         // Version string is matched in release.config.js
         // Changing this may result in breaking automated releases
-        internal static let Version: String = "0.0.0"
+        internal static let Version: String = "0.0.1"
         internal static let Library: String = "experiment-ios-client"
     }
 }
