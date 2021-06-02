2528093b05828d76921a8da7ad8aa73f7647244b
Brian Giori <brian.giori@amplitude.com>
2021-06-02 09:23:44 -0700

    feat: doc update

--- Sources/Experiment/ExperimentUserProvider.swift
+++ Sources/Experiment/ExperimentUserProvider.swift
@@ -1,5 +1,5 @@
 //
-//  ContextProvider.swift
+//  ExperimentUserProvider.swift
 //  Experiment
 //
 //  Copyright © 2020 Amplitude. All rights reserved.
@@ -7,6 +7,14 @@
 
 import Foundation
 
+/// Provides a user to an ExperimentClient to be merged without overwriting prior to
+/// fetching variants for the user. In otherwords, fields are only sent in the fetch request
+/// if the user object passed into fetch or stored by the ExperimentClient does not
+/// define those fields.
+///
+/// This is useful for providing contextual information about the platform, or pulling
+/// data which may potentially change without explicitly pass the data into the
+/// client on each fetch.
 public protocol ExperimentUserProvider {
     func getUser() -> ExperimentUser
 }
