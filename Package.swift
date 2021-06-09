713a9b832ca5783ff1dd7edd6a7d38ef1920826d
Brian Giori <brian.giori@amplitude.com>
2021-06-09 16:15:23 -0700

    fix: remove amplitude as dependency

--- Package.swift
+++ Package.swift
@@ -25,7 +25,6 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "Experiment",
-            dependencies: ["Amplitude"],
             path: "Sources/Experiment",
             exclude: ["Info.plist"]),
         .testTarget(
