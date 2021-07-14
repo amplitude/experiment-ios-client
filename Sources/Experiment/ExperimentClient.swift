7a44a5de6c26e4c1ec3c7bfc72c6dbfd728901f0
Brian Giori <brian.giori@amplitude.com>
2021-07-13 22:48:10 -0700

    fix: use post instead of get for fetch

--- Sources/Experiment/ExperimentClient.swift
+++ Sources/Experiment/ExperimentClient.swift
@@ -138,14 +138,15 @@ public class DefaultExperimentClient : ExperimentClient {
             completion(Result.failure(ExperimentError("json encode failed from dictionary: \(userDictionary)")))
             return nil
         }
-        let b64encodedUrl = requestData.base64EncodedString().replacingOccurrences(of: "+", with: "-")
-            .replacingOccurrences(of: "/", with: "_")
-            .replacingOccurrences(of: "=", with: "")
-        let url = URL(string: "\(self.config.serverUrl)/sdk/vardata/\(b64encodedUrl)")!
+        if requestData.count > 8000 {
+            print("[Experiment] encoded user object length \(requestData.count) cannot be cached by CDN; must be < 8KB")
+        }
+        let url = URL(string: "\(self.config.serverUrl)/sdk/vardata")!
         var request = URLRequest(url: url)
-        request.httpMethod = "GET"
+        request.httpMethod = "POST"
         request.setValue("Api-Key \(self.apiKey)", forHTTPHeaderField: "Authorization")
         request.timeoutInterval = Double(timeoutMillis) / 1000.0
+        request.httpBody = requestData
         
         // Do fetch request
         let task = URLSession.shared.dataTask(with: request) { (data, response, error) in
