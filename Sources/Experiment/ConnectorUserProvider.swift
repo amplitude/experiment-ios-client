113fcdcf187924f5607da96920dddbe7d91450d9
Brian Giori <brian.giori@amplitude.com>
2023-03-14 17:23:46 -0700

    fix: connector user provider concurrency (#30)

--- Sources/Experiment/ConnectorUserProvider.swift
+++ Sources/Experiment/ConnectorUserProvider.swift
@@ -8,39 +8,58 @@
 import Foundation
 import AnalyticsConnector
 
+func copyId(_ identity: Identity) -> Identity {
+    return Identity(userId: identity.userId, deviceId: identity.deviceId, userProperties: identity.userProperties)
+}
+
 internal class ConnectorUserProvider : ExperimentUserProvider {
     
     private let identityStore: IdentityStore
     private let baseUserProvider = DefaultUserProvider()
+    private var identity: Identity? = nil
+    private let identityLock = DispatchSemaphore(value: 1)
+    
+    private let id: String = randomString(length: 16)
     
     init(identityStore: IdentityStore) {
         self.identityStore = identityStore
+        self.identityStore.addIdentityListener(key: id) { (updatedId) in
+            self.identityLock.wait()
+            defer { self.identityLock.signal() }
+            self.identity = copyId(updatedId)
+        }
     }
     
+    
     func getUser() -> ExperimentUser {
-        let identity = self.identityStore.getIdentity()
+        self.identityLock.wait()
+        defer { self.identityLock.signal() }
         return baseUserProvider.getUser().copyToBuilder()
-            .userId(identity.userId)
-            .deviceId(identity.deviceId)
-            .userProperties(identity.userProperties as? [String:Any])
+            .userId(identity?.userId)
+            .deviceId(identity?.deviceId)
+            .userProperties(identity?.userProperties as? [String:Any])
             .build()
     }
     
     func getUserOrWait(timeout: DispatchTimeInterval) throws -> ExperimentUser {
-        let identity = try getIdentityOrWait(timeout: timeout)
+        self.identityLock.wait()
+        if identity == nil {
+            self.identityLock.signal()
+            try waitForIdentity(timeout: timeout)
+            self.identityLock.wait()
+        }
+        defer { self.identityLock.signal() }
         return baseUserProvider.getUser().copyToBuilder()
-            .userId(identity.userId)
-            .deviceId(identity.deviceId)
-            .userProperties(identity.userProperties as? [String:Any])
+            .userId(identity?.userId)
+            .deviceId(identity?.deviceId)
+            .userProperties(identity?.userProperties as? [String:Any])
             .build()
     }
     
-    private func getIdentityOrWait(timeout: DispatchTimeInterval) throws -> Identity {
-        let listenerId = self.randomString(length: 16)
+    private func waitForIdentity(timeout: DispatchTimeInterval) throws {
+        let listenerId = randomString(length: 16)
         let lock = DispatchSemaphore(value: 0)
-        var listenerIdentity = Identity()
         identityStore.addIdentityListener(key: listenerId) { (identity) in
-            listenerIdentity = identity
             lock.signal()
         }
         defer { identityStore.removeIdentityListener(key: listenerId) }
@@ -50,14 +69,11 @@ internal class ConnectorUserProvider : ExperimentUserProvider {
             if result == .timedOut {
                 throw ExperimentError("Timed out waiting for Amplitude Analytics SDK to initialize. You must ensure that the analytics SDK is initialized prior to calling fetch().")
             }
-            return listenerIdentity
-        } else {
-            return immediateIdentity
         }
     }
-    
-    private func randomString(length: Int) -> String {
-      let letters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
-      return String((0..<length).map{ _ in letters.randomElement()! })
-    }
+}
+
+private func randomString(length: Int) -> String {
+  let letters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
+  return String((0..<length).map{ _ in letters.randomElement()! })
 }
