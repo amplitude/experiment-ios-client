5d09ef6440e27dcca14c841c502e959237fc2362
Chris Leonavicius <chris.leonavicius@amplitude.com>
2025-05-09 10:33:36 -0700

    fix: allow multiple experiment plugins to attach to an instance of Amplitude

--- Package@swift-5.9.swift
+++ Package@swift-5.9.swift
@@ -21,7 +21,7 @@ let package = Package(
     dependencies: [
         // Dependencies declare other packages that this package depends on.
         .package(url: "https://github.com/amplitude/analytics-connector-ios.git", from: "1.3.1"),
-        .package(url: "https://github.com/amplitude/AmplitudeCore-Swift.git", from: "1.0.10"),
+        .package(url: "https://github.com/amplitude/AmplitudeCore-Swift.git", from: "1.0.11"),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
